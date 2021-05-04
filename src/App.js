1eca51fe9569b39eb44a7dd4d4660b81806fedce
vntvibr <vitor.brina@venturus.org.br>
2021-05-04 10:19:48 -0300

    Testing useCep Hook.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import useCep from "react-hook-usecep";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [cepStatus, setCep] = useCep();
+  const [cepData, setCepData] = useState();
+
+  useEffect(() => {
+    setCepData(cepStatus.data);
+  }, [cepStatus]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Digite o CEP abaixo</h1>
+        <input
+          className="App-input"
+          onBlur={(event) => setCep(event.target.value)}
+        />
+        {cepData && (
+          <div className="App-cep-infos">
+            <span>{cepData.logradouro}</span>
+            <span>{cepData.bairro}</span>
+            <span>{cepData.localidade}</span>
+            <span>{cepData.cep}</span>
+          </div>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
